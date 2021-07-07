a64e94392b0d11ca9ecce368ff1c428fbc4b269f
admin CIFO <admin CIFO@DESKTOP-8S89R17>
2021-07-07 13:07:28 +0200

    update crud operation

--- amazonDelivery4/src/main/java/example/boot/dev/employee1/EmployeeRestController.java
+++ amazonDelivery4/src/main/java/example/boot/dev/employee1/EmployeeRestController.java
@@ -6,25 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/webapi")
 public class EmployeeRestController {
-	
+
 	@Autowired
 	EmployeeRepository employeeRepository;
-	
+
 	@GetMapping("/allEmployees")
-	public  Iterable<Employee> getAllEnployees () {
-		
+	public Iterable<Employee> getAllEnployees() {
+
 		return employeeRepository.findAll();
-		
-		
-		
 	}
-	
+
 	@GetMapping("/getEmployee/{id}")
 	public Employee findById(@PathVariable int id) {
 
@@ -37,15 +37,47 @@ public class EmployeeRestController {
 
 		return null;
 	}
-	
+
 	@DeleteMapping("/deleteEmployee/{id}")
 	public void deleteEmployee(@PathVariable int id) {
 
 		employeeRepository.deleteById(id);
-
 	}
-	
-	
-	
+
+	@PostMapping(path = "/addEmployee", consumes = "application/json")
+	public void insertEmployee(@RequestBody Employee employee) {
+
+		// System.out.println(book);
+		employeeRepository.save(employee);
+	}
+
+	@PutMapping("/updateEmployee/{id}")
+	public void upadateEmployee(@RequestBody Employee employee, @PathVariable int id) {
+
+		Optional<Employee> employeeFound = employeeRepository.findById(id);
+
+		if (employeeFound.isPresent()) {
+
+			if (!employee.getName().equals(employeeFound.get().getName()))
+				employeeFound.get().setName(employee.getName());
+			
+			if (!employee.getSurname().equals(employeeFound.get().getSurname()))
+				employeeFound.get().setSurname(employee.getSurname());
+
+			if (employee.getAge() != employeeFound.get().getAge())
+				employeeFound.get().setAge(employee.getAge());
+
+			if (!employee.getEmail().equals(employeeFound.get().getEmail()))
+				employeeFound.get().setEmail(employee.getEmail());
+
+			if (employee.getMonthSalary() != employeeFound.get().getMonthSalary())
+				employeeFound.get().setMonthSalary(employee.getMonthSalary());
+
+			if (!employee.getPassword().equals(employeeFound.get().getPassword()))
+				employeeFound.get().setPassword(employee.getPassword());
+
+			employeeRepository.save(employeeFound.get());
+		}
+	}
 
 }
